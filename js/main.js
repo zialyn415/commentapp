bea630c6a6afe64e4625194ef3782c9f6ac67606
Krizia <krizialudovico@gmail.com>
2018-05-14 14:58:49 -0700

    updated

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-// function todoList() {
-// 	var username = document.getElementById("username").value
-// 	var comment = document.getElementById("comment").value
+function todoList() {
+	var username = document.getElementById("username").value
+	var comment = document.getElementById("comment").value
 
-// 	var userNameText = document.createTextNode("Username: " + username)
-// 	var commentText = document.createTextNode("Comment: " + comment)
+	var userNameText = document.createTextNode("Username: " + username)
+	var commentText = document.createTextNode("Comment: " + comment)
 
-// 	var newListItem = document.createElement("LI")
-// 	newListItem.className = "list-group-item"
-// 	var newUserName = document.createElement("H3")
-// 	newUserName.appendChild(userNameText)
-// 	var newComment = document.createElement("P")
-// 	newComment.appendChild(commentText)
+	var newListItem = document.createElement("LI")
+	newListItem.className = "list-group-item"
+	var newUserName = document.createElement("H3")
+	newUserName.appendChild(userNameText)
+	var newComment = document.createElement("P")
+	newComment.appendChild(commentText)
 
-// 	newListItem.appendChild(newUserName)
-// 	newListItem.appendChild(newComment)
-// 	document.getElementById("commentList").appendChild(newListItem)
-// }
+	newListItem.appendChild(newUserName)
+	newListItem.appendChild(newComment)
+	document.getElementById("commentList").appendChild(newListItem)
+}
